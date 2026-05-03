d4ca333ba51d89a9a0b920193d83d0c6dab7141c
ඔබගindikalogon <indikalogon@gmail.com>
2026-05-03 06:14:26 +0530

    17

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.rentcalo.com',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false, // කුඩා වෙබ් අඩවි සඳහා මෙය false කිරීම වඩාත් සුදුසුයි
+}
