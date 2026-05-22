b7b87ac25f2994f25dd9fb109b440f9b6059e9f2
ඔබගindikalogon <indikalogon@gmail.com>
2026-05-22 23:50:09 +0530

    29

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,6 +10,7 @@ export default function Document() {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
+      <meta name="google-adsense-account" content="ca-pub-3232905618503691"></meta>
       <body>
         <Main />
         <NextScript />
